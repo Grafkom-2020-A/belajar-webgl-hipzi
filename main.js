e3204484c83d30242e40246350b489e54dec5cf5
hipzi <zahramillah582@gmail.com>
2020-10-12 08:37:23 +0700

    jadi 3 titik

--- main.js
+++ main.js
@@ -2,22 +2,29 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 50.0;
-    }
-    `;
+    // Definisi vertex2 pada segotiga
+    /**
+     * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5);
+     */
+    var vertices = [
+        -0.5, 0.5,  // Titik A
+        -0.5, -0.5, // Titik B
+        0.5, -0.5   // Titik C
+    ];
+
+    // Buffer
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null); //memutus proses buffer
+
+    var vertexShaderCode = document.getElementById("vertexShaderSource").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main() {
-        gl_FragColor = vec4(.5, 0.5, 0.5, 1.0);
-    }
-    `;
+    var fragmentShaderCode = document.getElementById("fragmentShaderSource").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -29,8 +36,19 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    // Memetakan buffer CPU ke attribute
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position"); 
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0,0.5,1.0,1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    // Buat segitiga step(1)
+    var primitive = gl.POINTS;
+    var offset = 0;
+    var count = 3; // Julah vertex yg akan digambar
+
+    gl.drawArrays(gl.POINTS, offset, count);
 }
