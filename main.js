7e2f96632df8084781dad18e90a968aa2472c4e8
hipzi <zahramillah582@gmail.com>
2020-11-02 15:50:14 +0700

    tambah translasi

--- main.js
+++ main.js
@@ -59,8 +59,6 @@ function main() {
     gl.enableVertexAttribArray(aPosition);
     gl.enableVertexAttribArray(aColor);
 
-    gl.clearColor(0.0,0.5,1.0,1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
     gl.viewport(100, 0, canvas.height, canvas.height); 
 
     // Buat segitiga step(1)
@@ -68,5 +66,25 @@ function main() {
     var offset = 0;
     var count = 6; // Julah vertex yg akan digambar
 
-    gl.drawArrays(primitive, offset, count);
+    var dx = 0;
+    var dy = 0;
+    var dz = 0;
+    var uDx = gl.getUniformLocation(shaderProgram, 'dx');
+    var uDy = gl.getUniformLocation(shaderProgram, 'dy');
+    var uDz = gl.getUniformLocation(shaderProgram, 'dz');
+    // gl.uniform1f(uDz, dz); // Tidak perlu di dalam loop render, karena dz tidak berubah
+
+    function render(){
+        dx += 0.001;
+        dy += 0.001;
+        dz += 0.001;
+        gl.uniform1f(uDx, dx);
+        gl.uniform1f(uDy, dy);
+        gl.uniform1f(uDz, dz);
+        gl.clearColor(0.0,0.5,1.0,1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(primitive, offset, count);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
